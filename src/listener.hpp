da3e946227561d82010a29183856da6b1c81c100
Max Kryzhanovskiy <kryzhanovskiymax@mail.ru>
2023-03-11 09:31:42 +0300

    listener into file

--- src/listener.hpp
+++ src/listener.hpp
@@ -7,5 +7,55 @@ namespace beast = boost::beast;
 namespace sys = boost::system;
 namespace http = beast::http;
 
+template <typename RequestHandler>
+class Listener : public std::enable_shared_from_this<Listener<RequestHandler>> {
+public:
+    template <typename Handler>
+    Listener(net::io_context& ioc, const tcp::endpoint& endpoint, Handler&& request_handler)
+            : ioc_(ioc),
+              acceptor_(net::make_strand(ioc)),
+              request_handler_(std::forward<Handler>(request_handler)) {
+
+        acceptor_.open(endpoint.protocol()); // open acceptor to receive connections
+        acceptor_.set_option(net::socket_base::reuse_address(true)); 
+
+        acceptor_.bind(endpoint);
+        acceptor_.listen(net::socket_base::max_listen_connections);
+    }
+
+
+    void Run() {
+        DoAccept();
+    }
+private:
+    net::io_context& ioc_;
+    tcp::acceptor acceptor_;
+    RequestHandler request_handler_;
+
+    void DoAccept() {
+        acceptor_.async_accept(net::make_strand(ioc_), beast::bind_front_handler(&Listener::OnAccept, this->shared_from_this()));
+    }
+
+    void OnAccept(sys::error_code ec, tcp::socket socket) {
+        using namespace std::literals;
+
+        if (ec) {
+            return ReportError(ec, "accept"sv);
+        }
+
+        AsyncRunSession(std::move(socket));
+        DoAccept();
+    }
+
+    void AsyncRunSession(tcp::socket&& socket) {
+        std::make_shared<Session<RequestHandler>>(std::move(socket), request_handler_)->Run();
+    }
+};
+
+template <typename RequestHandler>
+void ServeHttp(net::io_context& ioc, const tcp::endpoint& endpoint, RequestHandler&& handler) {
+    using MyListener = Listener<std::decay_t<RequestHandler>>;
+    std::make_shared<MyListener>(ioc, endpoint, std::forward<RequestHandler>(handler))->Run();
+}
 
 }
