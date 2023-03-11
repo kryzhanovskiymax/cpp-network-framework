b43f35342b64545667b6fe7972cb5cf9a9e7284d
Max Kryzhanovskiy <kryzhanovskiymax@mail.ru>
2023-03-11 09:30:37 +0300

    session into file

--- src/session.hpp
+++ src/session.hpp
@@ -1,3 +1,11 @@
+#pragma once
+
+#include <boost/asio/signal_set.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
 
 namespace http {
 namespace net = boost::asio;
@@ -6,5 +14,93 @@ namespace beast = boost::beast;
 namespace sys = boost::system;
 namespace http = beast::http;
 
+class SessionBase {
+public:
+    SessionBase(const SessionBase&) = delete;
+    SessionBase& operator=(const SessionBase&) = delete;
+
+    void Run() {
+        net::dispatch(stream_.get_executor(), beast::bind_front_handler(&SessionBase::Read, GetSharedThis()));
+    }
+
+protected:
+    explicit SessionBase(tcp::socket&& socket): stream_(std::move(socket)) {}
+    using HttpRequest = http::request<http::string_body>;
+    ~SessionBase() = default;
+
+    template <typename Body, typename Fields>
+    void Write(http::response<Body, Fields>&& response) {
+        auto safe_response = std::make_shared<http::response<Body, Fields>>(std::move(response));
+        auto self = GetSharedThis();
+        http::async_write(stream_, *safe_response, [safe_response, self] (beast::error_code ec, std::size_t bytes_written) {
+            self->OnWrite(safe_response->need_eof(), ec, bytes_written);
+        });
+    }
+private:
+    beast::tcp_stream stream_;
+    beast::flat_buffer buffer_;
+    HttpRequest request_;
+
+    void Read() {
+        using namespace std::literals;
+        request_ = {};
+        stream_.expires_after(30s);
+
+        http::async_read(stream_, buffer_, request_, beast::bind_front_handler(&SessionBase::OnRead, GetSharedThis()));
+    }
+
+    void OnRead(beast::error_code ec, [[maybe_unused]] std::size_t bytes_read) {
+        using namespace std::literals;
+        if (ec == http::error::end_of_stream) {
+            return Close();
+        }
+
+        if (ec) {
+            return ReportError(ec, "read"sv);
+        }
+
+        HandleRequest(std::move(request_));
+    }
+
+    void OnWrite(bool close, beast::error_code ec, [[maybe_unused]] std::size_t bytes_written) {
+        if (ec) {
+            return ReportError(ec, "write"sv);
+        }
+
+        if (close) {
+            return Close();
+        }
+
+        Read();
+    }
+
+    void Close() {
+        beast::error_code ec;
+        stream_.socket().shutdown(tcp::socket::shutdown_send, ec);
+    }
+
+    virtual void HandleRequest(HttpRequest&& request) = 0;
+    virtual std::shared_ptr<SessionBase> GetSharedThis() = 0;
+};
+
+template <typename RequestHandler>
+class Session : public SessionBase, public std::enable_shared_from_this<Session<RequestHandler>> {
+public:
+    template <typename Handler>
+    Session(tcp::socket&& socket, Handler&& request_handler)
+        : SessionBase(std::move(socket)), request_handler_(request_handler) {}
+private:
+    RequestHandler request_handler_;
+
+    std::shared_ptr<SessionBase> GetSharedThis() override {
+        return this->shared_from_this();
+    }
+
+    void HandleRequest(HttpRequest&& request) override {
+        request_handler_(std::move(request), [self = this->shared_from_this()](auto&& response) {
+            self->Write(std::move(response));
+        });
+    }
+};
 
 }
