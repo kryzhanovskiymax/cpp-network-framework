c835570a1bddd41651703b0b19c0a55092210f76
Max Kryzhanovskiy <kryzhanovskiymax@mail.ru>
2023-03-11 09:38:47 +0300

    errrors specified

---
+++ src/error.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+
+namespace http {
+
+namespace beast = boost::beast;
+
+void ReportError(beast::error_code ec, std::string_view what) {
+    std::cerr << what << ": " << ec.message() << std::endl;
+}
+
+}
